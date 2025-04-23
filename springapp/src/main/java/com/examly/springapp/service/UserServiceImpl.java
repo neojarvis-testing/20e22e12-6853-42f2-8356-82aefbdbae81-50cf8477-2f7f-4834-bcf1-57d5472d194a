276a9ff5ed4be7db76cc23c22ed9103f6d73dd30
shiv-2 <arumugamg21068@gmail.com>
2025-04-23 14:26:19 +0000

    Register User

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,5 +1,28 @@
 package com.examly.springapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.examly.springapp.model.User;
+import com.examly.springapp.repository.UserRepo;
+
+@Service
 public class UserServiceImpl {
+
+    private UserRepo userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepo userRepo){
+        this.userRepo=userRepo;
+    }
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
 
 }
