8aff73177295ac8f442ad7d7bea1cd4746ff2c9e
viswakiran16 <viswakiran15@gmail.com>
2025-04-24 14:19:43 +0000

    Updated Appointment Service and Controller

--- springapp/src/main/java/com/examly/springapp/repository/AppointmentRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/AppointmentRepo.java
@@ -1,9 +1,21 @@
 package com.examly.springapp.repository;
-
+ 
+import java.util.List;
+ 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+ 
+import com.examly.springapp.model.Appointment;
+ 
 @Repository
-public interface AppointmentRepo extends JpaRepository<AppointmentRepo,Long>{
-
+public interface AppointmentRepo extends JpaRepository<Appointment,Long>{
+ 
+ 
+    @Query("select appointment from Appointment appointment where appointment.user.userId = ?1")
+    List<Appointment> findByUserId(int userId);
+ 
+    @Query("select appointment from Appointment appointment where appointment.user.userId = ?1 order by appointment.appointmentDate desc limit 1")
+    Appointment getLastAppointmentbyUserId(int userId);
+ 
 }
