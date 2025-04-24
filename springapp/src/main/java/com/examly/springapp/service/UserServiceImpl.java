3d5e6bb0e5bdca8754c359a1eb54f72cb1d0a397
shiv-2 <arumugamg21068@gmail.com>
2025-04-24 08:49:22 +0000

    Added spring security

--- springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/UserServiceImpl.java
@@ -1,28 +1,49 @@
 package com.examly.springapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.examly.springapp.config.UserPrinciple;
 import com.examly.springapp.model.User;
 import com.examly.springapp.repository.UserRepo;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserDetailsService {
 
     private UserRepo userRepo;
 
     @Autowired
-    public UserServiceImpl(UserRepo userRepo){
-        this.userRepo=userRepo;
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
     }
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private PasswordEncoder encoder;
 
     public User registerUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(encoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User existingUser = userRepo.findByEmail(email);
+        if (existingUser == null) {
+            throw new UsernameNotFoundException("User name not found");
+        }
+        return UserPrinciple.build(existingUser);
+    }
+
+    public User loginUser(User user) {
+        User existingUser = userRepo.findByEmail(user.getEmail());
+        if (existingUser == null) {
+            return null;
+        }
+        // No need to encode the password again here
+        return existingUser;
+    }
 }
