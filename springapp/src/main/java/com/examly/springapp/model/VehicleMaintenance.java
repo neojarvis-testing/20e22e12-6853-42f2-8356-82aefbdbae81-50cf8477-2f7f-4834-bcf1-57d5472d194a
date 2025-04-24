12ea9f9c738eeace09be007ce114f1e0171c7b7a
SiriChandana-21 <gollasirii@gmail.com>
2025-04-24 04:46:46 +0000

    VehicleMaintenance created

--- springapp/src/main/java/com/examly/springapp/model/VehicleMaintenance.java
+++ springapp/src/main/java/com/examly/springapp/model/VehicleMaintenance.java
@@ -1,5 +1,52 @@
 package com.examly.springapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class VehicleMaintenance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long serviceId;
+    private String serviceName;
+    private int servicePrice;
+    private String typeOfVehicle;
+    public long getServiceId() {
+        return serviceId;
+    }
+    public void setServiceId(long serviceId) {
+        this.serviceId = serviceId;
+    }
+    public String getServiceName() {
+        return serviceName;
+    }
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+    public int getServicePrice() {
+        return servicePrice;
+    }
+    public void setServicePrice(int servicePrice) {
+        this.servicePrice = servicePrice;
+    }
+    public String getTypeOfVehicle() {
+        return typeOfVehicle;
+    }
+    public void setTypeOfVehicle(String typeOfVehicle) {
+        this.typeOfVehicle = typeOfVehicle;
+    }
+    public VehicleMaintenance() {
+    }
+    public VehicleMaintenance(long serviceId, String serviceName, int servicePrice, String typeOfVehicle) {
+        this.serviceId = serviceId;
+        this.serviceName = serviceName;
+        this.servicePrice = servicePrice;
+        this.typeOfVehicle = typeOfVehicle;
+    }
+    
+
+
 
 }
