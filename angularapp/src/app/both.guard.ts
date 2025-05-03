0a62d3e169a37e8b05a3d67b80a8df96323d84e1
viswakiran16 <viswakiran15@gmail.com>
2025-05-03 05:09:37 +0000

    Sonar Code

--- angularapp/src/app/both.guard.ts
+++ angularapp/src/app/both.guard.ts
@@ -1,17 +1,31 @@
+
 import { Injectable } from '@angular/core';
+
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+
 import { AuthService } from './services/auth.service';
- 
+
+
+
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class BothGuard implements CanActivate {
+
   canActivate(
+
     route: ActivatedRouteSnapshot,
+
     state: RouterStateSnapshot): boolean {
-    return this.service.isUser() || this.service.isAdmin();
-  }
- 
+
+      return this.service.isUser() || this.service.isAdmin();
+
+    }
+
+    
   constructor(private readonly service:AuthService){}
  
 }
